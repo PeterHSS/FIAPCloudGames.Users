6e24d34b5748e3a495ed2475b722af633d3b7f4b
PeterHSS <pedro.henrique.ismart@gmail.com>
2026-01-25 02:25:23 -0300

    Move Serilog config from appsettings.json to code
    Serilog logging configuration has been removed from appsettings.json and is now set up programmatically in SerilogExtensions.cs. This includes setting minimum log levels, configuring console and OpenSearch sinks, and adding enrichers for context, machine, process, thread, environment, and application name. Added Serilog enrichment packages to the project file to support these enrichers.

--- FIAPCloudGames.Users.Api/Commom/Extensions/SerilogExtensions.cs
+++ FIAPCloudGames.Users.Api/Commom/Extensions/SerilogExtensions.cs
@@ -9,11 +9,34 @@ public static class SerilogExtensions
     {
         hostBuilder.UseSerilog((context, configuration) =>
         {
-            configuration.WriteTo.OpenSearch(new OpenSearchSinkOptions(new Uri("https://localhost:9200"))
+            Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine(msg));
+
+            configuration.WriteTo.Console(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information);
+
+            configuration.MinimumLevel.Information();
+
+            configuration
+                .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Error)
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Error)
+                .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Error);
+
+            configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessName()
+                .Enrich.WithThreadId()
+                .Enrich.WithThreadName()
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithProperty("Application", "FIAPCloudGames.Users.Api");
+
+            configuration.WriteTo.OpenSearch(new OpenSearchSinkOptions(new Uri(context.Configuration["Opensearch:Host"]!))
             {
                 AutoRegisterTemplate = true,
                 IndexFormat = "fgc-users-api-{0:yyyy.MM.dd}",
-                ModifyConnectionSettings = conn => conn.ServerCertificateValidationCallback((o, certificate, chain, errors) => true).BasicAuthentication(context.Configuration["Opensearch:Username"], context.Configuration["Opensearch:Password"])
+                ModifyConnectionSettings = conn => 
+                    conn
+                        .ServerCertificateValidationCallback((o, certificate, chain, errors) => true)
+                        .BasicAuthentication(context.Configuration["Opensearch:Username"], context.Configuration["Opensearch:Password"])
             });
         });
 
